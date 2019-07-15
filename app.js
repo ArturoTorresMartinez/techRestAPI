7289087385c8769d37c56eb9dd70152208820ba5
Arturo Torres <efrainarturotorres@gmail.com>
2019-07-14 22:23:25 -0500

    test

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const app = express();
+
+//Enabling cors from all origins!
+app.use(cors());
+//Parser to help me read the body of the requests!
+app.use(bodyParser.json());
+
+//configuring my graphQL options with graphiql for GUI and little error handler!
+app.use("/graphql", graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    customFormatErrorFn(err) {
+        if (!err.originalError) {
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || "An error occured";
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data };
+    }
+}));
+
+//General error handler!
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+});
+
+
+//Connecting with mongo database with newUrlParser and FindAndModify options so the console stops sending warnings!
+const MONGODB_URI = `mongodb+srv://arthur:poq7283ipod@cluster0-e7jon.mongodb.net/inno?retryWrites=true`;
+mongoose.connect(MONGODB_URI, { useFindAndModify: false, useNewUrlParser: true }).then(result => {
+    //Just making sure it runs on the service provider intended port or 3000!
+    app.listen(process.env.PORT || 3000);
+}).catch(err => console.log(err));
