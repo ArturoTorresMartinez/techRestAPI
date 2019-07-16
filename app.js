118313e90aad920e16597f673b00baf5df7c5a02
Arturo Torres <efrainarturotorres@gmail.com>
2019-07-15 20:28:01 -0500

    Final version

--- app.js
+++ app.js
@@ -14,38 +14,38 @@ app.use(bodyParser.json());
 
 //configuring my graphQL options with graphiql for GUI and little error handler!
 app.use(
-  "/graphql",
-  graphqlHttp({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true,
-    customFormatErrorFn(err) {
-      if (!err.originalError) {
-        return err;
-      }
-      const data = err.originalError.data;
-      const message = err.message || "An error occured";
-      const code = err.originalError.code || 500;
-      return { message: message, status: code, data: data };
-    }
-  })
+    "/graphql",
+    graphqlHttp({
+        schema: graphqlSchema,
+        rootValue: graphqlResolver,
+        graphiql: true,
+        customFormatErrorFn(err) {
+            if (!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || "An error occured";
+            const code = err.originalError.code || 500;
+            return { message: message, status: code, data: data };
+        }
+    })
 );
 
 //General error handler!
 app.use((error, req, res, next) => {
-  console.log(error);
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-  res.status(status).json({ message: message, data: data });
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 //Connecting with mongo database with newUrlParser and FindAndModify options so the console stops sending warnings!
-const MONGODB_URI = `mongodb+srv://arthur:poq7283ipod@cluster0-e7jon.mongodb.net/inno?retryWrites=true`;
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-e7jon.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`;
 mongoose
-  .connect(MONGODB_URI, { useFindAndModify: false, useNewUrlParser: true })
-  .then(result => {
-    //Just making sure it runs on the service provider intended port or 3000!
-    app.listen(process.env.PORT || 3000);
-  })
-  .catch(err => console.log(err));
+    .connect(MONGODB_URI, { useFindAndModify: false, useNewUrlParser: true })
+    .then(result => {
+        //Just making sure it runs on the service provider intended port or 3000!
+        app.listen(process.env.PORT || 3000);
+    })
+    .catch(err => console.log(err));
